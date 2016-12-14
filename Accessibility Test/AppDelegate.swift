88030722dd06610c9375d732a2438884edb23c85
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2016-12-14 17:45:24 +0100

    Initial Commit

---
+++ Accessibility Test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Accessibility Test
+//
+//  Created by Tomáš Znamenáček on 14/12/2016.
+//  Copyright © 2016 Tomáš Znamenáček. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
